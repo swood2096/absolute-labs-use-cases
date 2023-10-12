a344376c01cc86ab6f6157c3ca89de9e73af9c0f
swood2096 <121150288+swood2096@users.noreply.github.com>
2023-10-12 14:45:14 -0700

    Create backend.py

---
+++ backend.py
@@ -0,0 +1,9 @@
+absolute_labs_info = {
+    "features": ["feature1", "feature2", "feature3"],
+    "benefits": ["benefit1", "benefit2", "benefit3"],
+    # Add more info as needed
+}
+def generate_use_cases(company_details):
+    # Logic to generate use cases based on company details and absolute_labs_info
+    # For the sake of simplicity, we'll return a static example here
+    return f"{company_details['name']} can leverage {absolute_labs_info['features'][0]} for better performance."
