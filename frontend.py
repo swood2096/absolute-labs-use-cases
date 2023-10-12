7a0a85b9f3d4a993802ca7883d3355933a0c58d1
swood2096 <121150288+swood2096@users.noreply.github.com>
2023-10-12 15:07:46 -0700

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,13 +1,19 @@
+from flask import Flask, render_template, request
 import backend
 
-def main():
-    company_details = {
-        'name': input("Enter company name: "),
-        # Prompt for other details as needed
-    }
+app = Flask(__name__)
 
-    use_cases = backend.generate_use_cases(company_details)
-    print(use_cases)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        company_details = {
+            'name': request.form['company_name']
+            # Add other fields as needed
+        }
+        use_cases = backend.generate_use_cases(company_details)
+        return render_template('index.html', use_cases=use_cases)
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
+
